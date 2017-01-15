1ea409fdecbff2371814b4114ae57db0d505b390
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-15 19:58:15 +0000

    Overwrite if file exists

--- deploy_to_dropbox.py
+++ deploy_to_dropbox.py
@@ -4,4 +4,4 @@ import dropbox
 dbx = dropbox.Dropbox(os.environ['DROPBOX_TOKEN'])
 
 with open('./project.pdf', 'rb') as f:
-    dbx.files_upload(f.read(), '/Imperial/Work/301 Indiv. Proj./project.pdf')
+    dbx.files_upload(f.read(), '/Imperial/Work/301 Indiv. Proj./project.pdf', dropbox.files.WriteMode('overwrite', None))
