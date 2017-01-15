8b17d8986c2f75a5bb2dddf280af3a435752d0ff
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-15 00:01:48 +0000

    Correct error in python

--- deploy_to_dropbox.py
+++ deploy_to_dropbox.py
@@ -3,5 +3,5 @@ import dropbox
 
 dbx = dropbox.Dropbox(os.environ['DROPBOX_TOKEN'])
 
-with open('./project.pdf', 'rb'):
+with open('./project.pdf', 'rb') as f:
     dbx.files_upload(f, '/Imperial/Work/301 Indiv. Proj./project.pdf')
