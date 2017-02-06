0e05df1fe83eae39d53301c2c5a86a000606eb6a
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-02-06 12:23:56 +0000

    Make interim and project reports (travis)

--- deploy_to_dropbox.py
+++ deploy_to_dropbox.py
@@ -3,5 +3,8 @@ import dropbox
 
 dbx = dropbox.Dropbox(os.environ['DROPBOX_TOKEN'])
 
-with open('./project.pdf', 'rb') as f:
-    dbx.files_upload(f.read(), '/Imperial/Work/301 Indiv. Proj./project.pdf', dropbox.files.WriteMode('overwrite', None))
+files_to_upload = ['project.pdf', 'interim.pdf']
+
+for filename in files_to_upload:
+    with open(filename, 'rb') as f:
+        dbx.files_upload(f.read(), '/Imperial/Work/301 Indiv. Proj./' + filename, dropbox.files.WriteMode('overwrite', None))
