6884e5a468b08349aa3f1670eff76abd4bc7f4c3
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-02-06 13:41:59 +0000

    Run builds in parallel

--- deploy_to_dropbox.py
+++ deploy_to_dropbox.py
@@ -3,8 +3,7 @@ import dropbox
 
 dbx = dropbox.Dropbox(os.environ['DROPBOX_TOKEN'])
 
-files_to_upload = ['project.pdf', 'interim.pdf']
+filename = os.environ['UPLOAD_FILE']
 
-for filename in files_to_upload:
-    with open(filename, 'rb') as f:
-        dbx.files_upload(f.read(), '/Imperial/Work/301 Indiv. Proj./' + filename, dropbox.files.WriteMode('overwrite', None))
+with open(filename, 'rb') as f:
+    dbx.files_upload(f.read(), '/Imperial/Work/301 Indiv. Proj./' + filename, dropbox.files.WriteMode('overwrite', None))
