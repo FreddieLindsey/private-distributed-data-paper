122bb4d09fa0f87f272ea35404dc2436c1405c12
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-04-25 13:17:55 +0100

    Use branch name

--- .travis/deploy_to_dropbox.py
+++ .travis/deploy_to_dropbox.py
@@ -6,7 +6,7 @@ dbx = dropbox.Dropbox(os.environ['DROPBOX_TOKEN'])
 filename = os.environ['UPLOAD_FILE']
 
 if len(sys.argv) > 1:
-    filename = sys.argv[1] + '/' + filename
+    filename = sys.argv[1] + '.pdf'
 
 with open(os.environ['UPLOAD_FILE'], 'rb') as f:
     dbx.files_upload(f.read(), '/Imperial/Work/301 Indiv. Proj./' + filename, dropbox.files.WriteMode('overwrite', None))
