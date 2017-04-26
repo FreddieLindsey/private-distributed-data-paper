af277700b443bd80dc445dd053f6febb7479bf5f
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-04-26 11:27:43 +0100

    Build html before building pdf

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 LATEXMK="latexmk -f -pdf"
 
+PANDOC_ARGS="--verbose --toc --toc-depth=6 --mathjax --highlight-style espresso -s"
+PANDOC_HTML="pandoc --css .travis/notes.css --css .travis/highlight.css $PANDOC_ARGS"
+
 if [ $# -gt 0 ] && [ $1 == 'clean' ]; then
   $LATEXMK -C
 elif [ $# -gt 0 ]; then
+  $PANDOC_HTML $* -o project.html
   $LATEXMK $*
   $LATEXMK -c
 fi
