0f7e2ee75ea63cc28b84948c575e511464142265
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-04-26 11:29:21 +0100

    Remove toc from html

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 LATEXMK="latexmk -f -pdf"
 
-PANDOC_ARGS="--verbose --toc --toc-depth=6 --mathjax --highlight-style espresso -s"
+PANDOC_ARGS="--verbose --mathjax --highlight-style espresso -s"
 PANDOC_HTML="pandoc --css .travis/notes.css --css .travis/highlight.css $PANDOC_ARGS"
 
 if [ $# -gt 0 ] && [ $1 == 'clean' ]; then
